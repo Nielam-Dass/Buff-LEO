8662378871373be7c36b1b9aecc378ae88ea5250
Nielam Dass <nielam.dass@gmail.com>
2026-08-05 19:56:25 -0400

    Inject static content script into all frames

--- src/utils/contentScript.ts
+++ src/utils/contentScript.ts
@@ -1,5 +1,6 @@
 import bindings from "./keyBindings"
 
+
 for (const binding of bindings) {
     document.addEventListener("keydown", (e: KeyboardEvent): void => {
         if (binding.checkKeydownEvent(e)) binding.action()
