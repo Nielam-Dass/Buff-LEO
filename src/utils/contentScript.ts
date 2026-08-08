2fe007978cf9c7c7675f659c009e68ecf8481c73
Nielam Dass <nielam.dass@gmail.com>
2026-08-08 19:28:25 -0400

    Prevent default action when a keyboard shortcut is pressed

--- src/utils/contentScript.ts
+++ src/utils/contentScript.ts
@@ -3,6 +3,9 @@ import bindings from "./keyBindings"
 
 for (const binding of bindings) {
     document.addEventListener("keydown", (e: KeyboardEvent): void => {
-        if (binding.checkKeydownEvent(e)) binding.action()
+        if (binding.checkKeydownEvent(e)) {
+            e.preventDefault()
+            binding.action()
+        }
     })
 }
