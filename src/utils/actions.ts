5b4422d4f414cd2542c8b5458e327881c23737d5
Nielam Dass <nielam.dass@gmail.com>
2026-08-07 22:35:37 -0400

    Implement all action functions

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,27 +1,37 @@
-const blockHrefInline = (clickEvent: MouseEvent) => {
-    if (clickEvent.target instanceof HTMLAnchorElement && clickEvent.target.href==="javascript:void(0)") {
-        clickEvent.preventDefault()
-    }
-}
-
 export const activatePen = (): void => {
-    const penBtn: HTMLAnchorElement | null = document.querySelector<HTMLAnchorElement>("#toolConfigurationContainer > ul a[title='Pen']")
-    penBtn?.addEventListener("click", blockHrefInline)
+    const penBtn: HTMLElement | null = acquireToolBtn("Pen")
     penBtn?.click()
 }
 
 export const activateEraser = (): void => {
-    console.log("Eraser is active")
+    const eraserBtn: HTMLElement | null = acquireToolBtn("Eraser")
+    eraserBtn?.click()
 }
 
 export const activateTextBox = (): void => {
-    console.log("Text box is active")
+    const textBoxBtn: HTMLElement | null = acquireToolBtn("Text")
+    textBoxBtn?.click()
 }
 
 export const activatePointer = (): void => {
-    console.log("Pointer is active")
+    const pointerBtn: HTMLElement | null = acquireToolBtn("Pointer")
+    pointerBtn?.click()
 }
 
 export const activateHighlighter = (): void => {
-    console.log("Highlighter is active")
+    const highlighterBtn: HTMLElement | null = acquireToolBtn("Highlighter")
+    highlighterBtn?.click()
+}
+
+const acquireToolBtn = (toolType: string): HTMLElement | null => {
+    const selectorStr: string = `#toolConfigurationContainer > ul a[title="${toolType}"]`
+    const toolBtn: HTMLAnchorElement | null = document.querySelector<HTMLAnchorElement>(selectorStr)
+    toolBtn?.addEventListener("click", blockHrefInline)
+    return toolBtn
+}
+
+const blockHrefInline = (clickEvent: MouseEvent): void => {
+    if (clickEvent.target instanceof HTMLAnchorElement && clickEvent.target.href==="javascript:void(0)") {
+        clickEvent.preventDefault()
+    }
 }
